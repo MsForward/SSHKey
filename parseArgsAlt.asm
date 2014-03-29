ba8c678b46f9ce29aa8692130a1016b19ab72091
Beata Obrok <beata@obrok.eu>
2014-03-29 20:56:20 +0100

    Add lots of comments

--- parseArgsAlt.asm
+++ parseArgsAlt.asm
@@ -1,22 +1,26 @@
 ; Methods:
-;   parseArgs
-;   printArgs
-;   getArg
+;   drunkenBishop                                   Creates movement map for given key
+;   parseArgs                                       Saves command line arguments as array of string
+;   putAddress                                      Saves arguments offset in pointer array                                       
+;   printArgs                                       Prints all command line arguments
+;   getArg                                          Returns offset for argument with given index
 ;       entry: DL = argument index
 ;       return: AL = argument offset
-;   getArgLen
+;   getArgLen                                       Returns length of argument
 ;       entry: DL = argument index
 ;       return: AL = argument length
-;   getArgNum
+;   getArgNum                                       Returns number of command line arguments
 ;       return: AL = number of arguments
-;   checkArgs
+;   checkArgs                                       Checks if arguments are valid
 ;       return: AL = 1 if arguments valid else 0
-;   checkKey
+;   checkKey                                        Checks if SSH key is valid
 ;       return: AL = 1 if key valid else 0
-;   checkVersion
+;   convertKey                                      Converts SSH key from string to integer array
+;   checkVersion                                    Checks if version number is valid
 ;       return: AL = 1 if version valid else 0
-;   printError
-;       entry: DX = error offset
+;   print
+;       entry: DX = string offset                   Prints string to console
+;   crlf                                            Prints new line in console
 
 data segment
 ; Input
@@ -24,7 +28,10 @@ data segment
     argPtr          db 128 dup(0)   ; array of argument offsets
     argNum          db 0            ; stores number of arguments
     runV            db 0            ; version of algorithm to run
-    keyPtr          db 0            ; offset of SSH key 
+    key             db 16 dup(?)    ; numerical key
+    
+; Drunken bishop
+    array           db 152 dup(0)   ; szachownica 
     
 ; Error messages    
     argNumErr       db "Too many arguments! Usage: version key","$"
@@ -40,8 +47,111 @@ code segment
 start:
     call init
     call parseArgs
-    call printArgs
+    call checkArgs
+    call convertKey
+    call drunkenBishop
     call exit
+    
+    drunkenBishop proc
+        push ax
+        push bx
+        
+        mov si, offset key
+        mov di, offset array
+        ; set bishop at starting position
+        mov bx, 76d
+        
+        mov al, byte ptr ds:[di]
+        mov dl, al
+        call move
+        
+        pop bx
+        pop ax 
+        ret
+    drunkenBishop endp
+    
+    move proc
+    ; entry: DL = byte to convert
+        push ax
+        push cx
+        
+        ; read 2 bits at a time
+        moveLoop:
+            mov cx, 8h
+            ; copy number
+            mov al, dl
+            ; check last bit
+            and al, 1h
+            ; if 1 move right else left
+            cmp al, 1h
+            je moveRight
+            
+            moveLeft: ; x = x - 1
+                dec bx    
+            moveRight: ; x = x + 1
+                inc bx
+            
+            ; mark move 
+            inc ds:[array + bx]    
+            
+            ; get next bit    
+            shr al, 1
+            and al, 1h
+            ; if 1 move down else up
+            cmp al, 1h
+            je moveDown
+            
+            moveUp: ; y = y - 17
+                sub bx, 17d
+            moveDown: ; y = y + 17
+                add bx, 17d    
+            
+            ; mark move
+            inc ds:[array + bx]
+            shr al, 1
+            dec cx
+            cmp cx, 0
+            jg moveLoop
+        
+        pop cx
+        pop ax
+        ret
+    move endp
+    
+    convertKey proc
+        push ax
+        
+        xor ax, ax
+        
+        mov dl, 1h
+        call getArg
+        mov si, ax
+        mov di, offset key
+        convertChar:
+            mov al, ds:[si]
+            cmp al, '$'
+            je endConvertKey
+            inc si
+            mov ah, ds:[si]
+            shl ah, 2h
+            add al, ah
+            add al, 30h
+            cmp al, 9h
+            jle cvcLoop
+            
+            letter:
+                sub al, 7h
+                            
+            cvcLoop:        
+                mov ds:[di], al
+                inc si
+                inc di
+                jmp convertChar
+                    
+        endConvertKey:        
+        pop ax
+        ret
+    convertKey endp
     
     checkArgs proc
         push bx
@@ -68,22 +178,25 @@ start:
         call checkKey
         cmp al, 1h
         jne endCheckArgs
+        call checkVersion
+        cmp al, 1h
+        jmp endCheckArgs
         
         wrongArgNum:
             mov dx, offset argNumErr
-            call printError
+            call print
             xor al, al
             jmp endCheckArgs
             
         wrongArgLen:
             mov dx, offset argLenErr
-            call printError
+            call print
             xor al, al
             jmp endCheckArgs
         
         wrongVersion:
             mov dx, offset versionErr
-            call printError
+            call print
             xor al, al
             jmp endCheckArgs
         
@@ -109,6 +222,7 @@ start:
         call getArg
         mov bl, al
         mov dl, al
+
         add dx, 20h
         
         checkChar:
@@ -126,7 +240,7 @@ start:
         
         invalidChar:
             mov dx, offset keyCharErr
-            call printError
+            call print
             ; return false
             xor al, al
             jmp endCheckKey
@@ -134,8 +248,8 @@ start:
         ccLoop:
             inc bx
             cmp bx, dx
-            jle checkChar
-        
+            jl checkChar
+            
         ; return true 
         mov al, 1h
         endCheckKey:
@@ -144,14 +258,6 @@ start:
         pop bx
         ret
     checkKey endp
-    
-    printError proc
-        push ax
-        mov ah, 9h
-        int 21h
-        pop ax
-        ret
-    printError endp
     
     checkVersion proc
         push ax
@@ -283,7 +389,7 @@ start:
             inc bl
             
             ret
-    endp        
+    readArg endp        
     
     putAddress proc
         push bx
@@ -294,7 +400,7 @@ start:
         
         pop bx
         ret
-    readArg endp
+    putAddress endp
     
     getArg proc
     ; entry: DL = argument index 
@@ -312,18 +418,16 @@ start:
     
     getArgLen proc 
     ; entry : DL = argument index
-    ; return: AL = arugment length
+    ; return: AL = argument length
     
-        push ax
         push bx   
         
         ; clear address storage
         xor bx, bx
         ; dl stores argument index
         call getArg
-        ; save first offset
-        mov al, 1
-        mov bl, dl
+        mov bl, al
+        mov al, 1h
         getChar:
             ; go to next character 
             inc bx
@@ -339,7 +443,6 @@ start:
         endGetArgLen:
         
         pop bx
-        pop ax
         ret
     getArgLen endp
     
@@ -368,8 +471,7 @@ start:
             call getArg
             ; print argument to console
             mov dl, al
-            mov ah, 9h
-            int 21h
+            call print
             ; print line break
             call crlf
             
@@ -383,6 +485,14 @@ start:
         pop ax
         ret
     printArgs endp
+    
+    print proc
+        push ax
+        mov ah, 9h
+        int 21h
+        pop ax
+        ret
+    print endp
     
     crlf proc ; prints line break 
         push ax
